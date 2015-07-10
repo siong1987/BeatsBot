199e00acf1cd85cad42377aa628c5c64872908de
Siong <siong1987@gmail.com>
2015-07-10 00:12:09 -0700

    use the right redis.

--- model/base.rb
+++ model/base.rb
@@ -1,7 +1,13 @@
 require 'sinatra/activerecord'
 
 require 'redis'
-$redis = Redis.new(url: ENV['REDIS_PROVIDER'])
+
+if ENV['REDISCLOUD_URL']
+  uri = URI.parse(ENV['REDISCLOUD_URL'])
+  $redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
+else
+  $redis = Redis.new
+end
 
 require './model/webhook'
 require './model/beat'
